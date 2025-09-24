180affdf742324aab8e8be2901753766d45225c7
Abhinav Sharma <114858652+AbhinavSharma486@users.noreply.github.com>
2025-09-24 17:28:04 +0530

    added Complaint Management routes

--- backend/routes/admin.route.js
+++ backend/routes/admin.route.js
@@ -1,9 +1,19 @@
 import express from "express";
 import { protectAdminRoute } from "../middleware/admin.auth.middleware";
-import { getSystemStats } from "../controllers/admin.controller.js";
+import {
+  deleteComplaintAdmin,
+  getAllComplaintsAdmin,
+  getComplaintByIdAdmin,
+  getSystemStats
+} from "../controllers/admin.controller.js";
 
 
 const router = express.Router();
 
 // Admin Dashboard and Analytics 
 router.get("/analytics", protectAdminRoute, getSystemStats);
+
+// Complaint Management (Admin)
+router.get("/complaints", protectAdminRoute, getAllComplaintsAdmin);
+router.get("/complaints/:id", protectAdminRoute, getComplaintByIdAdmin);
+router.delete("/complaints/:id", protectAdminRoute, deleteComplaintAdmin);
