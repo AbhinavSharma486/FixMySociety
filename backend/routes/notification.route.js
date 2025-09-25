65b53892f9bd457f30fea8be53f6a213c70e8dc8
Abhinav Sharma <114858652+AbhinavSharma486@users.noreply.github.com>
2025-09-25 13:21:50 +0530

    added All Notifications routes

---
+++ backend/routes/notification.route.js
@@ -0,0 +1,20 @@
+import express from "express";
+import { userprotectRoute } from "../middleware/user.auth.middleware.js";
+import {
+  deleteNotification,
+  getNotificationStats,
+  getUserNotifications,
+  markNotificationAsRead,
+  markNotificationAsUnread
+} from "../controllers/notification.controller.js";
+
+const router = express.Router();
+
+// All notificatoin routes are protected 
+router.get("/user", userprotectRoute, getUserNotifications);
+router.get("/stats", userprotectRoute, getNotificationStats);
+router.put("/:notificationId/read", userprotectRoute, markNotificationAsRead);
+router.put("/:notificationId/unread", userprotectRoute, markNotificationAsUnread);
+router.delete("/:notificationId", userprotectRoute, deleteNotification);
+
+export default router;
