0550495129b9fb4866b28903db7bff25162395cc
Abhinav Sharma <114858652+AbhinavSharma486@users.noreply.github.com>
2025-09-25 10:02:42 +0530

    added All Building routes

---
+++ backend/routes/building.route.js
@@ -0,0 +1,22 @@
+import express from "express";
+import { protectAdminRoute } from "../middleware/admin.auth.middleware.js";
+import {
+  createBuilding,
+  deleteBuilding,
+  getAllBuildings,
+  getBuildingById,
+  getBuildingStats,
+  updateBuilding
+} from "../controllers/building.controller.js";
+
+const router = express.Router();
+
+// All building routes are admin protected
+router.post("/create", protectAdminRoute, createBuilding);
+router.get("/all", protectAdminRoute, getAllBuildings);
+router.get("/:id", protectAdminRoute, getBuildingById);
+router.put("/:id", protectAdminRoute, updateBuilding);
+router.delete("/:id", protectAdminRoute, deleteBuilding);
+router.get("/:id/stats", protectAdminRoute, getBuildingStats);
+
+export default router;
