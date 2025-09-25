cd8e78f38f200a48cf13f30bb5372fa19a548313
Abhinav Sharma <114858652+AbhinavSharma486@users.noreply.github.com>
2025-09-25 13:30:26 +0530

    added Search Routes

---
+++ backend/routes/search.route.js
@@ -0,0 +1,11 @@
+import express from "express";
+import { userprotectRoute } from "../middleware/user.auth.middleware.js";
+import { getSearchFilters, globalSearch } from "../controllers/search.controller.js";
+
+const router = express.Router();
+
+// Search routes (protected)
+router.get("/global", userprotectRoute, globalSearch);
+router.get("/filters", userprotectRoute, getSearchFilters);
+
+export default router;
