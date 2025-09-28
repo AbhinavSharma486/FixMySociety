7500a8fb9ae256ffb9030030dfcad52379061828
Abhinav Sharma <114858652+AbhinavSharma486@users.noreply.github.com>
2025-09-29 04:30:18 +0530

    added Status Config File

---
+++ frontend/src/constants/statusConfig.js
@@ -0,0 +1,7 @@
+export const statusConfig = {
+  Pending: { color: 'badge-warning', label: 'Pending' },
+  'In Progress': { color: 'badge-info', label: 'In Progress' },
+  Resolved: { color: 'badge-success', label: 'Resolved' },
+};
+
+export default statusConfig;
