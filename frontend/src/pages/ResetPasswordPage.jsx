d7281bfee43cdd41b896f2181bf2faa866535987
Abhinav Sharma <114858652+AbhinavSharma486@users.noreply.github.com>
2025-10-25 13:15:01 +0530

    Fix UI issue of Reset Password Page

--- frontend/src/pages/ResetPasswordPage.jsx
+++ frontend/src/pages/ResetPasswordPage.jsx
@@ -87,7 +87,7 @@ const ResetPasswordPage = () => {
   }, []);
 
   return (
-    <div className="min-h-screen flex items-center justify-center p-4 bg-black relative overflow-hidden">
+    <div className="min-h-screen flex items-center justify-center p-4 bg-black relative overflow-hidden pt-16 xs:pt-17 sm:pt-18 lg:pt-20 xl:pt-24">
 
       {/* Animated Grid Background */}
       <div className="absolute inset-0 opacity-20 pointer-events-none">
